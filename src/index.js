b766f2776be9be36236e82374b33f5e553967ce9
Alex Marinkevich <bitrate595@gmail.com>
2022-05-06 16:41:06 +0300

    Setup MongoDb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 
+const dbURI = process.env.DB_URI;
 const app = express();
 const port = 3000;
+
+mongoose.connect(dbURI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listen at port: http://localhost:${port}`);
+    });
+  })
+  .catch((err) => console.error(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Home');
 });
 
-app.listen(port, () => `listen on port ${port}`);
+app.get('/:username', (req, res) => {
+  res.send('User page');
+});
