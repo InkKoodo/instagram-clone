1df0ccfe037922a18a42bf1907a0d4435e5041ac
Alex Marinkevich <bitrate595@gmail.com>
2022-05-09 01:40:16 +0300

    Add User model

---
+++ src/resources/user/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    bio: {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      occupation: {
+        type: String,
+        default: '',
+      },
+      profileDescription: {
+        type: [String],
+        default: [''],
+      },
+    },
+    followers: {
+      type: [mongoose.SchemaTypes.ObjectId],
+      ref: 'user',
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
