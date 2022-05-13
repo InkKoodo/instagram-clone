a7dc379597c1323a83b8661326c5edff0a4a4d3d
Alex Marinkevich <bitrate595@gmail.com>
2022-05-13 17:44:16 +0300

    refactor User router

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -7,33 +7,27 @@ const router = express.Router();
 // /user/:userId
 // get one User
 router.get('/:userId', async (req, res) => {
-  const user = await User.findById(req.params.userId).populate(['followers', 'following']);
-  res.json({
-    data: user,
-  });
+  const user = await User.findById(req.params.userId)
+    .populate(['followers', 'following']);
+  res.json({ data: user });
 });
 
 // /user/
 // createUser
 router.post('/', async (req, res) => {
+  const { username } = req.body;
   try {
-    const { username, firstName, lastName } = req.body;
     // check if user exists already
-
+    const ifUserExists = await User.findOne({ username });
+    if (ifUserExists) {
+      res.status(404).json({ errMessage: 'User already exists' });
+    }
     // crypt password (add preSave hook later to model)
 
     // save user
-    const newUser = new User({
-      bio: {
-        username,
-        firstName,
-        lastName,
-      },
-    });
+    const newUser = await User.create(req.body);
 
-    const saveUser = await newUser.save();
-
-    res.status(200).json({ data: saveUser });
+    res.status(200).json({ data: newUser });
   } catch (err) {
     res.status(400).json({ data: err });
   }
