15f0b835760fd734a5b8f52483987aafcbcee761
Alex Marinkevich <bitrate595@gmail.com>
2022-07-04 20:37:01 +0300

    Introduce "Edit collection" feature

--- src/resources/user/userCollection/userCollection.router.js
+++ src/resources/user/userCollection/userCollection.router.js
@@ -8,9 +8,9 @@ router.route('/')
   // create collection
   .post(async (req, res) => {
     try {
-      const user = req.jwtData.userId;
+      const { userId } = req.jwtData;
       const newCollection = await UserCollection.create({
-        ownerId: user,
+        owner: userId,
         ...req.body,
       });
       return res.status(200).json({ data: newCollection });
@@ -20,8 +20,31 @@ router.route('/')
   });
 
 router.route('/:id')
-  // edit collection base properties
-  .put(async (req, res) => res.send('user collections'))
+
+  // edit collection
+  .put(async (req, res) => {
+    try {
+      const collectionId = req.params.id;
+      const { userId } = req.jwtData;
+      const { owner } = await UserCollection.findById(collectionId);
+
+      if (userId !== owner.toString()) {
+        return res.status(400).json({ error: { message: 'Only owner can change this settings' } });
+      }
+
+      const updatedCollection = await UserCollection.findByIdAndUpdate(
+        collectionId,
+        req.body,
+        {
+          new: true,
+          populate: ['owner', 'media', 'coEditors'],
+        },
+      );
+      return res.status(200).json({ data: updatedCollection });
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+  })
 
   // get collection by its id
   .get(async (req, res) => res.send('user collections'))
@@ -32,5 +55,9 @@ router.route('/:id')
 // add to collection
 
 // remove from collection
+
+// add co-editors
+
+// remove co-editors
 
 export default router;
