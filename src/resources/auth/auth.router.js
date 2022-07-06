354559825c60f50a8c51ff036e9348e80a23426b
Alex Marinkevich <bitrate595@gmail.com>
2022-07-06 13:08:48 +0300

    Change auth routes paths

--- src/resources/auth/auth.router.js
+++ src/resources/auth/auth.router.js
@@ -4,8 +4,8 @@ import { createUser, verifyUser } from '../user/user.controllers';
 
 const router = express.Router();
 
-router.post('/sign-up', (req, res) => createUser(req, res));
+router.post('/register', (req, res) => createUser(req, res));
 
-router.post('/sign-in', (req, res) => verifyUser(req, res));
+router.post('/login', (req, res) => verifyUser(req, res));
 
 export default router;
