2c7c44fb79173400c8263f453115e1c34ccce0d9
Alex Marinkevich <bitrate595@gmail.com>
2022-07-30 15:19:04 +0300

    Move User's controllers to its appropriate place

--- src/resources/user/user.controllers.js
+++ src/resources/user/user.controllers.js
@@ -59,3 +59,105 @@ export const verifyUser = async (req, res) => {
     return res.status(400).json({ error: e });
   }
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate(['followers', 'subscriptions']);
+
+    res.status(200).json({ data: user });
+  } catch (e) {
+    res.status(400).json({ error: e });
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).json({ data: users });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+};
+
+export const updateBio = async (req, res) => {
+  try {
+    const { userId } = req.jwtData;
+    const { bio } = await User.findById(userId);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        bio: {
+          ...bio,
+          ...req.body,
+        },
+      },
+      { new: true },
+    );
+    return res.status(200).json({ data: { updatedUser } });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  // todo: add "post deletion data query" to remove all its user data from other users
+  try {
+    const { userId } = req.jwtData;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(400).json({ error: { message: 'There\'s no such account' } });
+    }
+    return res.status(200).json({ data: { message: 'Account was deleted' } });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+};
+
+export const subscribe = async (req, res) => {
+  try {
+    const { subscriptionId } = req.body;
+    const { userId } = req.jwtData;
+
+    // check if user with this id exists
+    const isUserExists = await User.findById(req.params.id);
+    if (!isUserExists) return res.status(400).json({ error: { message: 'There\'s no such user' } });
+
+    // add account to follow
+    await User.findByIdAndUpdate(
+      userId,
+      { $addToSet: { subscriptions: subscriptionId } },
+    );
+
+    // add subscriber to a subscribed account
+    await User.findByIdAndUpdate(
+      subscriptionId,
+      { $addToSet: { followers: userId } },
+    );
+    return res.status(200).json({ data: { message: 'Successfully subscribed' } });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+};
+
+export const unsubscribe = async (req, res) => {
+  try {
+    const { subscriptionId } = req.body;
+    const { userId } = req.jwtData;
+
+    // remove account from subscriptions
+    await User.findByIdAndUpdate(
+      userId,
+      { $pull: { subscriptions: subscriptionId } },
+    );
+
+    // remove subscriber from followers
+    await User.findByIdAndUpdate(
+      subscriptionId,
+      { $pull: { followers: userId } },
+    );
+    return res.status(200).json({ data: { message: 'Successfully unsubscribed' } });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+};
--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -1,7 +1,10 @@
 import express from 'express';
 
-import User from './user.model';
 import userCollectionRouter from './userCollection/userCollection.router';
+import {
+  deleteUser, getAllUsers, getUser,
+  subscribe, unsubscribe, updateBio,
+} from './user.controllers';
 
 const router = express.Router();
 
@@ -9,110 +12,20 @@ router.use('/collections', userCollectionRouter);
 
 router.route('/')
   // get all users
-  .get(async (req, res) => {
-    try {
-      const users = await User.find({});
-      return res.status(200).json({ data: users });
-    } catch (e) {
-      return res.status(400).json({ error: e });
-    }
-  })
+  .get((req, res) => getAllUsers(req, res))
 
   // update user's bio
-  .put(async (req, res) => {
-    try {
-      const { userId } = req.jwtData;
-      const { bio } = await User.findById(userId);
-      const updatedUser = await User.findByIdAndUpdate(
-        userId,
-        {
-          bio: {
-            ...bio,
-            ...req.body,
-          },
-        },
-        { new: true },
-      );
-      return res.status(200).json({ data: { updatedUser } });
-    } catch (e) {
-      return res.status(400).json({ error: e });
-    }
-  })
+  .put((req, res) => updateBio(req, res))
 
   // delete user
-  .delete(async (req, res) => {
-    // todo: add "post deletion data query" to remove all its user data from other users
-    try {
-      const { userId } = req.jwtData;
-      const deletedUser = await User.findByIdAndDelete(userId);
-      if (!deletedUser) {
-        return res.status(400).json({ error: { message: 'There\'s no such account' } });
-      }
-      return res.status(200).json({ data: { message: 'Account was deleted' } });
-    } catch (e) {
-      return res.status(400).json({ error: e });
-    }
-  });
+  .delete((req, res) => deleteUser(req, res));
 
 router.route('/:id')
   // get User
-  .get(async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id)
-        .populate(['followers', 'subscriptions']);
+  .get((req, res) => getUser(req, res));
 
-      res.status(200).json({ data: user });
-    } catch (e) {
-      res.status(400).json({ error: e });
-    }
-  });
+router.post('/subscribe', (req, res) => subscribe(req, res));
 
-router.post('/subscribe', async (req, res) => {
-  try {
-    const { subscriptionId } = req.body;
-    const { userId } = req.jwtData;
-
-    // check if user with this id exists
-    const isUserExists = await User.findById(req.params.id);
-    if (!isUserExists) return res.status(400).json({ error: { message: 'There\'s no such user' } });
-
-    // add account to follow
-    await User.findByIdAndUpdate(
-      userId,
-      { $addToSet: { subscriptions: subscriptionId } },
-    );
-
-    // add subscriber to a subscribed account
-    await User.findByIdAndUpdate(
-      subscriptionId,
-      { $addToSet: { followers: userId } },
-    );
-    return res.status(200).json({ data: { message: 'Successfully subscribed' } });
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-});
-
-router.post('/unsubscribe', async (req, res) => {
-  try {
-    const { subscriptionId } = req.body;
-    const { userId } = req.jwtData;
-
-    // remove account from subscriptions
-    await User.findByIdAndUpdate(
-      userId,
-      { $pull: { subscriptions: subscriptionId } },
-    );
-
-    // remove subscriber from followers
-    await User.findByIdAndUpdate(
-      subscriptionId,
-      { $pull: { followers: userId } },
-    );
-    return res.status(200).json({ data: { message: 'Successfully unsubscribed' } });
-  } catch (e) {
-    return res.status(400).json({ error: e });
-  }
-});
+router.post('/unsubscribe', (req, res) => unsubscribe(req, res));
 
 export default router;
